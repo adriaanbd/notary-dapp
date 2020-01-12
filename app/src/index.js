0bc7a6056fa9a176b73aae974b26e550e56f284e
Adriaan Beiertz <adriaanbd@gmail.com>
2020-01-12 14:40:51 -0500

    removing comments

--- app/src/index.js
+++ app/src/index.js
@@ -10,7 +10,6 @@ const App = {
     const { web3 } = this;
 
     try {
-      // get contract instance
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = starNotaryArtifact.networks[networkId];
       this.meta = new web3.eth.Contract(
@@ -18,7 +17,6 @@ const App = {
         deployedNetwork.address,
       );
 
-      // get accounts
       const accounts = await web3.eth.getAccounts();
       this.account = accounts[0];
     } catch (error) {
@@ -39,7 +37,6 @@ const App = {
     App.setStatus("New Star Owner is " + this.account + ".");
   },
 
-  // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
     const { lookUptokenIdToStarInfo } = this.meta.methods;
     const id = document.getElementById("starId").value;
@@ -53,12 +50,10 @@ window.App = App;
 
 window.addEventListener("load", async function() {
   if (window.ethereum) {
-    // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     await window.ethereum.enable(); // get permission to access accounts
   } else {
     console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live",);
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"),);
   }
 
